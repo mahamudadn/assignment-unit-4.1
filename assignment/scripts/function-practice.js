e0798202c18e89dbba1e097b335a2e644964637b
Mahamud Aden <mahamudadn@gmail.com>
2023-04-18 14:25:32 -0500

    Added Hello Jo

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,16 +14,18 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName(  ) {
+  return ' Hello, Jo!';
 }
 // Remember to call the function to test
 
+console.log(helloName());
+
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
-}
+
+
+console.log('I have added two numbers', addNumbers)
 
 
 // 4. Function to multiply three numbers & return the result
