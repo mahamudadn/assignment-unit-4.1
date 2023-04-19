43a64f618007c2d6a9d59c9da96eda524704a5e1
Mahamud Aden <mahamudadn@gmail.com>
2023-04-19 15:23:07 -0500

    added value & returned true

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -44,15 +44,15 @@ console.log('I have multiplied three numbers(3, 5, 7:', numbersMultiplied);
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 
-
 function isPositive( number ) {
   if ( number > 0 ){
     return true; 
-  } else{
+  } else {
     return false;
   }
    
 }
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -62,16 +62,42 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+function getLast( array) {
 
+  console.log('name of arrays', array);
+   
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return arr[array.length -1];
+  }
+  
 }
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
+
+const myArray = [5, 7, 9];
+let value = 5;
+let found = find(value, myArray)
+
+console.log(found);
+console.log();
+
 function find( value, array ){
+  for(let i = 0; i<array.length; i++ ) {
+    if (array[i] === value ) {
+      return true;
+    } else{
+      return false;
+    }
+  }
   
 }
+
+
 
 // ----------------------
 // Stretch Goals
