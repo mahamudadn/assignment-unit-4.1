8ee853e94c6e1b8d448adffc50335c7e08b55055
Mahamud Aden <mahamudadn@gmail.com>
2023-04-23 17:15:48 -0500

    some text alteration

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -55,8 +55,11 @@ function isPositive( number ) {
 
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
+
 console.log( 'isPositive - should say true', isPositive(3) );
+
 console.log( 'isPositive - should say false', isPositive(0) );
+
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
@@ -105,30 +108,3 @@ function find( value, array ){
 
 
 
-// ----------------------
-// Stretch Goals
-// ----------------------
-// 8. Function to check if a letter is the first letter in a 
-//    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
-
-}
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
-
-// 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
-  return sum;
-}
-
-// 10. Function to return a new array of all positive (greater than zero)
-//     numbers contained in an input array. If there are no positive numbers
-//     return an empty array. Note: The input array should not change.
-
-
-
-// 11. Pick a problem from Edabit(https://edabit.com/) or 
-//     CodeWars(https://www.codewars.com/). Then describe it 
-//     here in a comment, write the function, and test it!
