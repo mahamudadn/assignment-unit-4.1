093b800f4c7b0f6b8bcc197489b376d3821750ca
Mahamud Aden <mahamudadn@gmail.com>
2023-04-23 17:27:32 -0500

    indented the file

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -5,16 +5,21 @@ console.log('***** Function Practice *****')
 // to test it and display the result
 
 // 1. Function to return 'Hello World!'
+
 function hello() {
+
   return 'Hello World!';
 }
 // Call the function to test
+
 console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+
 function helloName( name ) {
+
   return ' Hello, ' + name;
 }
 // Remember to call the function to test
@@ -25,19 +30,25 @@ console.log(helloName('joe'));
 // 3. Function to add two numbers together & return the result
 
 function addNumbers( firstNumber, secondNumber ) {
+
    return  firstNumber + secondNumber;
-}
+} 
+
 let numbersAdded = addNumbers(8, 10)
+
 console.log('I have added two numbers(8 & 10):', numbersAdded);
 
 
 // 4. Function to multiply three numbers & return the result
 
 function multiplyThree(number1, number2, number3 ){
+
   return number1 * number2 * number3;
 
 }
+
 let numbersMultiplied = multiplyThree(3, 5, 10);
+
 console.log('I have multiplied three numbers(3, 5, 10:', numbersMultiplied);
 
 
@@ -45,9 +56,13 @@ console.log('I have multiplied three numbers(3, 5, 10:', numbersMultiplied);
 //    or greater than zero, and false otherwise
 
 function isPositive( number ) {
+
   if ( number > 0 ){
+
     return true; 
+
   } else {
+
     return false;
   }
    
@@ -68,19 +83,27 @@ console.log( 'isPositive - should say false', isPositive(-3) );
  
 
 const emptyArray = [];
+
 const array3 = [2, 5, 9];
 
   function getLast(array) {
     
     if (array.length === 0) {
+
       return undefined;
+
     } else {
+      
       return array[array.length - 1];
     }
  }
+
  const lastItem = getLast(emptyArray);
+
  const finalitem = getLast(array3);
+
  console.log('The return of the last value is',lastItem, '', 'because the array is empty');
+
  console.log('The return of the last value is',finalitem, '', 'because the array has avalue');
 
 
@@ -89,16 +112,23 @@ const array3 = [2, 5, 9];
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 
+
 const myArray2 = [5, 7, 9];
+
 let value = 7;
+
 let found = find(value, myArray2)
 
  console.log('The variable value  is 7 and checks that number in the array, if it finds it returns:', found);
 
 function find( value, array ){
+
   for(let i = 0; i<array.length; i++ ) {
+
     if (array[i] === value ) {
+
       return true;
+
     } else {
       
     }
@@ -113,11 +143,14 @@ function find( value, array ){
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
+
 function isFirstLetter(letter, string) {
+
   return string.charAt(0) === letter;
 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 
@@ -142,12 +175,17 @@ console.log('The sum of numbers in the aaray are:', total);
 //     return an empty array. Note: The input array should not change.
 
 const inputNumbers = [2, -7, 8, 4, -5];
+
 const positiveNumbers = allPositiveNumbers(inputNumbers);
 
 function allPositiveNumbers(inputArray) {
+
   let PositiveNumbers = [];
+
   for(let i = 0; i < inputArray.length; i++ ) {
+
     if(inputArray[i] > 0) {
+
       PositiveNumbers.push (inputArray[i])
     }
   }
@@ -159,12 +197,17 @@ console.log('here are my positive numbers in  array ', positiveNumbers);
 // created another function that returns negative number in one array.
 
 const allNumbers = [2, -7, 8, 4, -5];
+
 const negativeNumbers = allNegativeNumbers(allNumbers);
 
 function allNegativeNumbers(inputArray) {
+
   let NegativeNumbers = [];
+
   for(let i = 0; i < inputArray.length; i++ ) {
+
     if(inputArray[i] < 0) {
+      
       NegativeNumbers.push(inputArray[i])
     }
   }
